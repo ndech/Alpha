ad95d42c36883147f51779078f61a9559c79e7af
Nicolas Dechamps <nicolas.dechamps@outlook.com>
2013-12-22 16:17:21 +0100

    Add State class

---
+++ PlaneSimulator/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaneSimulator.Toolkit.Math;
+
+namespace PlaneSimulator
+{
+    class State
+    {
+        public Vector3 Position {get; set;}
+        public Vector3 Speed { get; set; }
+        public Vector3 Acceleration { get; set; }
+        public Vector3 AngularPosition { get; set; }
+        public Vector3 AngularSpeed { get; set; }
+        public Vector3 AngularAcceleration { get; set; }
+    }
+}
